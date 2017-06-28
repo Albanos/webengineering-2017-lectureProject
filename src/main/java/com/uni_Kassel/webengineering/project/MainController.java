0d31fd06b87ce8e4566e6efd2f20b1135e750586
Luan Hajzeraj <luan.hajzeraj@student.uni-kassel.de>
2017-06-28 19:55:13 +0200

    Entfernen des Sprig Plugins

--- src/main/java/com/uni_Kassel/webengineering/project/MainController.java
+++ src/main/java/com/uni_Kassel/webengineering/project/MainController.java
@@ -2,7 +2,6 @@ package com.uni_Kassel.webengineering.project;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
